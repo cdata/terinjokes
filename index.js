e8211007917d863b2523eb22cc7d95adfb9de2ee
Chris Joel <chris@dabohealth.com>
2013-11-20 11:46:48 -0800

    Think of the children!
    Cheers :)

--- index.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-var jokes = require('./jokes.json');
-var random = require('lodash.random');
-
-module.exports = function terinjokes() {
-	return jokes[random(0, jokes.length-1)];
-};
